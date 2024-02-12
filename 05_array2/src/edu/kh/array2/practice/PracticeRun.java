f1b8b11f4e59cff494f532d7469abe7548d4a595
lhg010423 <lhg010423@gmail.com>
2024-02-12 23:17:30 +0900

    240212

--- 05_array2/src/edu/kh/array2/practice/PracticeRun.java
+++ 05_array2/src/edu/kh/array2/practice/PracticeRun.java
@@ -10,7 +10,11 @@ public class PracticeRun {
 //		ap.practice3();
 //		ap.practice4();
 //		ap.practice5();
-		ap.practice6();
+//		ap.practice6();
+//		ap.practice7();
+//		ap.practice8();
+//		ap.practice9();
+		ap.practice10();
 		
 		
 		
