485324c85bc0797469e8b10b6c516581a04709f2
lhg010423 <lhg010423@gmail.com>
2024-03-13 17:45:44 +0900

    240313

---
+++ Practice/src/programmers/level1/Test1.java
@@ -0,0 +1,19 @@
+package programmers.level1;
+
+public class Test1 {
+	
+    public int solution(String t, String p) {
+        int answer = 0;
+        int len = p.length();
+        
+        for(int i = 0; i < tLength-pLength+1; i++) {
+        	if()
+        	
+        	
+        }
+        
+        
+        
+        return answer;
+    } 
+}
---
+++ Practice/src/programmers/level1/Test1Run.java
@@ -0,0 +1,10 @@
+package programmers.level1;
+
+public class Test1Run {
+	public static void main(String[] args) {
+		
+		Test1 t1 = new Test1();
+		t1.solution("3141592", "271");
+		
+	}
+}
